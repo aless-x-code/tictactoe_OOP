2866ca1655533de5347bccb6b9b83614bb32c4dd
Alessandro <106855795+aless-x-code@users.noreply.github.com>
2023-10-25 21:59:28 -0400

    Update Game.h

--- Game.h
+++ Game.h
@@ -12,8 +12,10 @@ public:
     const int run_game(void);
 
 private:
+    const char valid_cell(void) const; // check that input is a valid cell
+    const bool is_not_equal(char &element, char &input) const;
     void print_grid(void) const;  // see cpp file for explanation
-    void tick_grid(const char &, const Player &);
+    void tick_grid(char &, const Player &);
     const int check_win(const Player &) const;
     const char restart_game(void) const;
     
@@ -21,9 +23,7 @@ private:
     char grid[3][3] = {0};      // 2d array, which will be the tic tac toe's grid
     int rounds = 1;             // variables stores and counts number of rounds. 1 round is defined as when both players make a single mark on the board, in other words, one X and one O is marked
     Player * players_Ptr[2];    // Array will store two pointers, which points to the 2 players playing the game
-    
-   
-    
+    vector<char> cells_occupied; // records the occupied board cells from the current game
 };
 
 
